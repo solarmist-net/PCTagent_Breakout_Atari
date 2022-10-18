3cbd7a23b381e195c52a24ffcb53e05fd59aa9ea
Joshua Olson <joshua.olson+github@gmail.com>
2022-10-18 09:43:37 -0700

    Change to Unix line endings

--- PCT_agent_clean.py
+++ PCT_agent_clean.py
@@ -16,7 +16,8 @@ Created on Sat Jun  5 23:11:13 2021
 
 #!/usr/bin/env python
 import sys
-sys.path.append('D:/Anaconda/envs/spyder/Lib/site-packages')
+
+sys.path.append("D:/Anaconda/envs/spyder/Lib/site-packages")
 import gym, time, cv2
 import numpy as np
 import collections
@@ -28,13 +29,13 @@ from percept_multi import percept_multi
 from percept_double import percept_double
 from percept_double_up import percept_double_up
 
-env = gym.make('BreakoutNoFrameskip-v4' if len(sys.argv)<2 else sys.argv[1])
+env = gym.make("BreakoutNoFrameskip-v4" if len(sys.argv) < 2 else sys.argv[1])
 
 # To save Rewards
 # outF = open("results_Breakout-V4_500.txt", "a")
 
-if not hasattr(env.action_space, 'n'):
-    raise Exception('Keyboard agent only supports discrete action spaces')
+if not hasattr(env.action_space, "n"):
+    raise Exception("Keyboard agent only supports discrete action spaces")
 ACTIONS = env.action_space.n
 SKIP_CONTROL = 0
 human_agent_action = 0
@@ -44,20 +45,24 @@ human_sets_pause = False
 
 def key_press(key, mod):
     global human_agent_action, human_wants_restart, human_sets_pause
-    if key==0xff0d: 
+    if key == 0xFF0D:
         human_wants_restart = True
-    if key==32: 
+    if key == 32:
         human_sets_pause = not human_sets_pause
-    a = int( key - ord('0') )
-    if a <= 0 or a >= ACTIONS: return
+    a = int(key - ord("0"))
+    if a <= 0 or a >= ACTIONS:
+        return
     human_agent_action = a
+
 
 def key_release(key, mod):
     global human_agent_action
-    a = int( key - ord('0') )
-    if a <= 0 or a >= ACTIONS: return
+    a = int(key - ord("0"))
+    if a <= 0 or a >= ACTIONS:
+        return
     if human_agent_action == a:
         human_agent_action = 0
+
 
 env.render()
 env.unwrapped.viewer.window.on_key_press = key_press
@@ -71,6 +76,7 @@ y_platei = collections.deque([0] * 2, maxlen=2)
 
 no_of_games = 0
 
+
 def rollout(env):
     global human_agent_action, human_wants_restart, human_sets_pause
     human_wants_restart = False
@@ -79,7 +85,7 @@ def rollout(env):
     total_reward = 0
     total_timesteps = 0
     itr = 0
-    
+
     # Game's Main Loop
     while 1:
         if not skip:
@@ -99,7 +105,7 @@ def rollout(env):
 
         # Generate Points of the Detected Objects
         if len(contours_ball) == 0:
-            print('Nothing is Detecte Something Wrong')
+            print("Nothing is Detecte Something Wrong")
             continue
         elif len(contours_ball) == 1:
             x_cent_plate, y_cent_plate, w_plate = bbox(contours_ball[0])
@@ -116,9 +122,9 @@ def rollout(env):
             x_platei.append(x_cent_plate)
             y_platei.append(y_cent_plate)
             # # Perceptual Ray Tracing
-            B = ray_tracing(x_ball, y_ball, x_balli[0],y_balli[0])
+            B = ray_tracing(x_ball, y_ball, x_balli[0], y_balli[0])
             # Distance Between Ball and the Plate
-            cpoint_dist = (w_plate/2) - 0.25
+            cpoint_dist = (w_plate / 2) - 0.25
             # Ball and Paddle Previous Positions
             bx = B[0]
             fx = x_cent_plate
@@ -126,32 +132,38 @@ def rollout(env):
             R1 = 0
             # All Gains
             k1, k2, k3, k4 = -1, 1, 1, 1
-            dist_vib = (w_plate/2)
+            dist_vib = w_plate / 2
             dist_vib1 = 6
-            
+
             if total_reward > -1:
-                # If Ball going Down 
-                if ( ((94-y_ball) - (94-y_balli[0]) < 0) ):
+                # If Ball going Down
+                if (94 - y_ball) - (94 - y_balli[0]) < 0:
                     # Multi-Hierarchical PCT Mode
                     # human_agent_action, itr = percept_multi(bx,fx,itr,R1,cpoint_dist,k1,k2,k3,k4)
                     # Double-Hierarchical Mode (Requires T=Ray Tracing)
-                    human_agent_action, itr = percept_double_up(bx,fx,itr,dist_vib1)
+                    human_agent_action, itr = percept_double_up(bx, fx, itr, dist_vib1)
 
                 # Image Processing Needs to be done
                 # If Ball Going up Track it
                 bx1 = 70
-                if (((94-y_ball) - (94-y_balli[0])) > 0):
-                    human_agent_action, itr = percept_double(bx1,itr,dist_vib,x_cent_plate)
+                if ((94 - y_ball) - (94 - y_balli[0])) > 0:
+                    human_agent_action, itr = percept_double(
+                        bx1, itr, dist_vib, x_cent_plate
+                    )
 
-        if window_still_open==False: return False
-        if done: break
-        if human_wants_restart: break
+        if window_still_open == False:
+            return False
+        if done:
+            break
+        if human_wants_restart:
+            break
         while human_sets_pause:
             env.render()
             time.sleep(0.1)
-        
+
         time.sleep(0.01)
     print("timesteps %i reward %0.2f" % (total_timesteps, total_reward))
+
 
 print("ACTIONS={}".format(ACTIONS))
 print("Press keys 1 2 3 ... to take actions 1 2 3 ...")
@@ -161,7 +173,7 @@ while 1:
     no_of_games = no_of_games + 1
     window_still_open = rollout(env)
     print(no_of_games)
-    if window_still_open==False: 
+    if window_still_open == False:
         # outF.close()
         cv2.destroyAllWindows()
         break
--- percept_multi.py
+++ percept_multi.py
@@ -7,12 +7,13 @@ Created on Wed Jul 28 17:18:33 2021
 
 import numpy as np
 
-def percept_multi(bx,fx,itr,R1,cpoint_dist,k1,k2,k3,k4):
+
+def percept_multi(bx, fx, itr, R1, cpoint_dist, k1, k2, k3, k4):
     global human_agent_action
-    
+
     # First Perceptual Rference
     # Distance Control
-    D = abs(bx - fx)+5 # NoFrameskipe 405
+    D = abs(bx - fx) + 5  # NoFrameskipe 405
     e1 = R1 - D
     R2 = e1 * k1
     # Movement Control
@@ -20,27 +21,27 @@ def percept_multi(bx,fx,itr,R1,cpoint_dist,k1,k2,k3,k4):
     e2 = R2 - MD
     if MD < 0:
         human_agent_action = 3
-        R3 = e2*k2
+        R3 = e2 * k2
         # Position Control
         e3 = R3 - fx
-        R4 = e3*k3
+        R4 = e3 * k3
         e4 = R4 + bx
-        BP = e4*k4
+        BP = e4 * k4
         itr = itr + 1
         if itr > 1 or (R2 < cpoint_dist) or BP == bx:
-              human_agent_action = 0
-              itr = 0
+            human_agent_action = 0
+            itr = 0
     if MD > 0:
         human_agent_action = 2
-        R3 = e2*k2
+        R3 = e2 * k2
         # Position Control
         e3 = R3 + fx
-        R4 = e3*k3
+        R4 = e3 * k3
         e4 = R4 - bx
-        BP = e4*k4
+        BP = e4 * k4
         itr = itr + 1
         if itr > 1 or (R2 < cpoint_dist) or BP == bx or fx > 136:
-              human_agent_action = 0
-              itr = 0
-              
+            human_agent_action = 0
+            itr = 0
+
     return human_agent_action, itr
